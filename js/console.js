0d1915068049a9c45ad5baeedced29240dccdff3
Beatriz Felix <beatrizfelix@aluno.fapce.edu.br>
2018-07-02 03:18:48 -0300

    transferindo projeto

---
+++ js/console.js
@@ -0,0 +1,17 @@
+//
+// console.groupCollapsed("nossas mensagens")
+// console.log("escrevendo no console");
+// console.error("eu sou um erro");
+// console.warn("eu sou um aviso");
+// console.assert("msg de erro");
+// console.groupEnd();
+//
+// console.table([
+//     [1,2,3],
+//     [2,4,6]
+// ])
+//
+// console.table([
+//     {nome:"Alura", num:10},
+//     {nome: "Bea", num:5}
+// ])
